e0542ca51d61f5cafe11633ebc1abb7f2a446919
eggegg <eggegg01@126.com>
2016-11-24 08:22:01 +0000

    now run phpunit multiple times and the tests will past everytime. Yay!

--- tests/app/Http/Controllers/BooksControllerTest.php
+++ tests/app/Http/Controllers/BooksControllerTest.php
@@ -3,11 +3,15 @@
 namespace Tests\App\Http\Controllers;
 
 use TestCase;
+use Laravel\Lumen\Testing\DatabaseMigrations;
 
 //use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class BooksControllerTest extends TestCase
 {
+
+    use DatabaseMigrations;
+
 //    /**
 //     * A basic test example.
 //     *
@@ -31,24 +35,27 @@ class BooksControllerTest extends TestCase
     /** @test */
     public function index_should_return_a_collection_of_records()
     {
-        $this->get('/books')
-            ->seeJson([
-                'title' => 'War of the Worlds'
-            ])
-            ->seeJson([
-                'title' => 'A Wrinkle in Time'
-            ]);
+        $books = factory('App\Book', 2)->create();
+
+        $this->get('/books');
+
+        foreach ($books as $book) {
+            $this->seeJson(['title' => $book->title]);
+        }
     }
 
     /** @test */
     public function show_should_return_a_valid_book()
     {
-        $this->get('/books/1')
+        $book = factory('App\Book')->create();
+
+        $this->get("/books/{$book->id}")
             ->seeStatusCode(200)
             ->seeJson([
-                'title' => 'War of the Worlds',
-                'description' => 'A science fiction masterpiece about Martians invading London',
-                'author' => 'H. G. Wells',
+                'id' => $book->id,
+                'title' => $book->title,
+                'description' => $book->description,
+                'author' => $book->author,
             ]);
 
         $data = json_decode($this->response->getContent(), true);
@@ -109,11 +116,13 @@ class BooksControllerTest extends TestCase
     /** @test */
     public function update_should_only_change_fillable_fields()
     {
-        $this->notSeeInDatabase('books', [
-            'title' => 'The War of the Worlds'
+        $book = factory('App\Book')->create([
+            'title' => 'The War of the Worlds',
+            'description' => 'The book is way better than the movie.',
+            'author' => 'Wells, H. G.'
         ]);
 
-        $this->put('/books/1',[
+        $this->put("/books/{$book->id}",[
             'id' => 5,
             'title' => 'The War of the Worlds',
             'description' => 'The book is way better than the movie.',
@@ -154,11 +163,12 @@ class BooksControllerTest extends TestCase
     /** @test */
     public function destroy_should_remove_a_valid_book()
     {
-        $this->delete('/books/1')
+        $book = factory('App\Book')->create();
+        $this->delete("/books/{$book->id}")
             ->seeStatusCode(204)
             ->isEmpty();
 
-        $this->notSeeInDatabase('books', ['id' => 1]);
+        $this->notSeeInDatabase('books', ['id' => $book->id]);
     }
 
     /** @test */
